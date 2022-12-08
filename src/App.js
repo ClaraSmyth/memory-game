9318cd179db8ffbe0eb224c1546e5b9741d028c5
Clara Smyth <clarasmyth.work@gmail.com>
2022-12-08 14:26:53 +0000

    Added a background video and image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import backgroundVideo from './assets/background.mp4';
+import backgroundImage from './assets/background.png';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div>Test</div>
+      <video id="background-video" autoPlay loop muted poster={backgroundImage}>
+        <source src={backgroundVideo} type="video/mp4" />
+      </video>
+    </div>
+  );
 }
 
 export default App;
