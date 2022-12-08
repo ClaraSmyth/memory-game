84d3f226438ff312925a4e238c060b1a369dbbe2
Clara Smyth <clarasmyth.work@gmail.com>
2022-12-08 15:25:25 +0000

    Added 2 new components

---
+++ src/components/Cards.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Cards() {
+  return (
+    <div className="card-container">
+      <div></div>
+    </div>
+  );
+}
+
+export default Cards;
---
+++ src/components/Scoreboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Scoreboard() {
+  return (
+    <div className="scoreboard-container">
+      <div></div>
+    </div>
+  );
+}
+
+export default Scoreboard;
