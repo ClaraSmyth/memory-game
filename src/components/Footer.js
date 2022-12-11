5026df4b9b7787928fdb9e8c60595135ecab00e6
Clara Smyth <clarasmyth.work@gmail.com>
2022-12-11 16:28:23 +0000

    Added a footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BsGithub } from 'react-icons/bs';
+
+function Footer() {
+  return (
+    <a className="footer-link" href="https://github.com/ClaraSmyth" target="_blank" rel="noreferrer">
+      <p>Clara Smyth</p>
+      <BsGithub />
+    </a>
+  );
+}
+
+export default Footer;
