386c98fbb3d72e8e9fcf4b87f4c2338fdaad6839
Clara Smyth <clarasmyth.work@gmail.com>
2022-12-23 16:09:00 +0000

    Updated CharArr with new images

--- src/utilities/charArr.js
+++ src/utilities/charArr.js
@@ -1,19 +1,14 @@
-import placeholder from '../assets/400.svg';
+function importAll(r) {
+  return r.keys().map(r);
+}
 
-const characters = [
-  { name: 'Luffy', image: placeholder },
-  { name: 'Nami', image: placeholder },
-  { name: 'Zoro', image: placeholder },
-  { name: 'Usopp', image: placeholder },
-  { name: 'Sanji', image: placeholder },
-  { name: 'Chopper', image: placeholder },
-  { name: 'Robin', image: placeholder },
-  { name: 'Franky', image: placeholder },
-  { name: 'Brook', image: placeholder },
-  { name: 'Jinbe', image: placeholder },
-  { name: 'Vivi', image: placeholder },
-  { name: 'Zeus', image: placeholder },
-  { name: 'Karoo', image: placeholder },
-];
+const images = importAll(require.context('../assets/OnePieceCharacterImages/', false, /\.(webp)$/));
+
+const characters = images.map((path) => {
+  return {
+    name: path.split(/[., /]/)[3],
+    image: path,
+  };
+});
 
 export default characters;
