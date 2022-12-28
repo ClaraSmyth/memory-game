3128b336b4e22593faea12fedb4bc1d1736fbf4a
Clara Smyth <clarasmyth.work@gmail.com>
2022-12-28 05:12:47 +0000

    Added GameOver modal to App

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import backgroundVideo from './assets/background.mp4';
 import backgroundImage from './assets/background.png';
 import { charArr } from './utilities';
 import { useState } from 'react';
-import { Header, Scoreboard, Cards, Footer } from './components/';
+import { Header, Scoreboard, Cards, Footer, GameOver } from './components/';
 
 function App() {
   const [characters, setCharacters] = useState(charArr);
@@ -11,15 +11,24 @@ function App() {
   const [highScore, setHighScore] = useState(0);
   const [canPlayAudio, setCanPlayAudio] = useState(1);
 
+  const resetGame = () => {
+    setScore(0);
+    setSelected([]);
+    setCharacters(charArr);
+  };
+
   const onSelection = (obj) => {
     // Check if game should be reset
-    const reset = selected.some((object) => object.name === obj.name);
+    if (selected.some((object) => object.name === obj.name)) {
+      resetGame();
+      return;
+    }
 
-    setScore(reset ? 0 : score + 1);
-    setSelected(reset ? [] : [...selected, obj]);
-    setCharacters(reset ? charArr : [...characters.filter((char) => char.name !== obj.name)]);
+    setScore(score + 1);
+    setSelected([...selected, obj]);
+    setCharacters([...characters.filter((char) => char.name !== obj.name)]);
 
-    if (score === highScore && !reset) setHighScore(highScore + 1);
+    if (score === highScore) setHighScore(highScore + 1);
   };
 
   return (
@@ -28,7 +37,11 @@ function App() {
 
       <Scoreboard score={score} highScore={highScore} />
 
-      <Cards characters={characters} selected={selected} onSelection={onSelection} canPlayAudio={canPlayAudio} />
+      {selected.length === charArr.length ? (
+        <GameOver resetGame={resetGame} />
+      ) : (
+        <Cards characters={characters} selected={selected} onSelection={onSelection} canPlayAudio={canPlayAudio} />
+      )}
 
       <Footer canPlayAudio={canPlayAudio} setCanPlayAudio={setCanPlayAudio} />
 
